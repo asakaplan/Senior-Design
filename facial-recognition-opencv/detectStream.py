b63dee7c207466b5d8ccf3a780b270a3fdd6ca60
Christopher Butler <Christopher Butler>
2017-02-21 03:39:55 -0500

    Removed additional facial recognition boxes when template matching

--- facial-recognition-opencv/detectStream.py
+++ facial-recognition-opencv/detectStream.py
@@ -8,26 +8,33 @@ cascPath = sys.argv[1]
 templatePath = sys.argv[2]
 faceCascade = cv2.CascadeClassifier(cascPath)
 template = cv2.imread(templatePath, 0)
-w, h = template.shape[::-1]
+testSizes = [120, 240, 480]
+xTemp = 0
+yTemp = 0
+threshold = 0.3
 
 video_capture = cv2.VideoCapture(0)
 
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
-
+    xTemp = 0
+    yTemp = 0
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Match a Template
-    res = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.40
-    loc = np.where(res >= threshold)
-    for pt in zip(*loc[::-1]):
-        cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+    for size in testSizes:
+        res = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
+        loc = np.where(res >= threshold)
+        for point in zip(*loc[::-1]):
+            cv2.rectangle(frame, point, (point[0] + 250, point[1] + 305), (0, 0, 255), 2)
+            xTemp = point[0]
+            yTemp = point[1]
+            #theimage = cv2.getRectSubPix(frame, (point[0], point[1]), (250, 305))
 
     faces = faceCascade.detectMultiScale(
        gray,
-       scaleFactor=1.1,
+       scaleFactor=1.15,
        minNeighbors=3,
        minSize=(15, 15),
        flags=cv2.CASCADE_SCALE_IMAGE
@@ -35,7 +42,8 @@ while True:
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 0), 2)
+        if xTemp < 1 or (((x < xTemp) or (x > xTemp + 325)) and ((y < yTemp) or (y > yTemp + 325))):
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 255), 2)
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
@@ -44,5 +52,7 @@ while True:
         break
 
 # When everything is done, release the capture
+#theimage = cv2.getRectSubPix(frame,(400, 500),(10, 10))
+#cv2.imwrite('person.png', theimage)
 video_capture.release()
 cv2.destroyAllWindows()
